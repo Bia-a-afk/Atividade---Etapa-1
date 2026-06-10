fe64e8c94f5dce67d8f7b7c95899d16e46c31fbb
Bia-a-afk <205434245+Bia-a-afk@users.noreply.github.com>
2026-06-10 10:58:55 -0300

    Add files via upload

--- Pagamento.java
+++ Pagamento.java
@@ -1,34 +1,36 @@
 public class Pagamento {
-    private double valor;
+   /* private double valor;
     private String metodoPagamento;
 
     public Pagamento(double valor, String metodoPagamento) {
         this.valor = valor;
         this.metodoPagamento = metodoPagamento;
-    }
+    }*/
 
-    public double getValor() {
+    /* public double getValor() {
         return valor;
     }
 
     public String getMetodoPagamento() {
-        return metodoPagamento;
+       return metodoPagamento;
+    }*/
+    public void processarPagamento(double valor) {
+        // Lógica para processar o pagamento
     }
-
     //PIX
     System.out.println("Pagamento via Pix selecionado.");
-    System.out.print("Valor a ser pago: R$ " + valor);
+    System.out.print("Valor a ser pago: R$ ");
     double valorPix = entrada.nextDouble();
     
-    System.out.println("Processando pagamento via Pix no valor de: " + valor);
+    System.out.println("Processando pagamento via Pix no valor de: " + valorPix );
     // Lógica para processar o pagamento via Pix
 
     //Boleto
     System.out.println("Pagamento via boleto selecionado.");
 
-    System.out.print("Valor a ser pago: R$ " + valor);
+    System.out.print("Valor a ser pago: R$ ");
     double valorBoleto = entrada.nextDouble();
 
-    System.out.println("Processando pagamento via boleto no valor de: " + valor);
+    System.out.println("Processando pagamento via boleto no valor de: "+ valorBoleto);
     // Lógica para processar o pagamento via boleto
 }
