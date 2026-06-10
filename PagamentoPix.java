e281ae0dfb9331fe0aeabe7625c097b6c9aef46c
hugoteixeira-creator <hugo.teixeira@undf.edu.br>
2026-06-10 11:15:38 -0300

    Update PagamentoPix.java

--- PagamentoPix.java
+++ PagamentoPix.java
@@ -1,5 +1,5 @@
-public class PagamentoPix implements Pagamento {
-    @Override
+public class PagamentoPix {
+    
     public void processarPagamento(double valor) {
         System.out.println("Processando pagamento via Pix no valor de: " + valor);
         // Lógica para processar o pagamento via Pix
