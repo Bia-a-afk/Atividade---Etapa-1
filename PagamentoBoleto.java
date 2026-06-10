3ebef19e6a1a03729969faaae9f79aa483064f54
hugoteixeira-creator <hugo.teixeira@undf.edu.br>
2026-06-10 11:14:53 -0300

    Update PagamentoBoleto.java

--- PagamentoBoleto.java
+++ PagamentoBoleto.java
@@ -1,6 +1,6 @@
-public class PagamentoBoleto implements Pagamento {
-    @Override
-    public void processarPagamento(double valor) {
+public class PagamentoBoleto  {
+    
+	public void processarPagamento(double valor) {
         System.out.println("Processando pagamento via boleto no valor de: " + valor);
         // Lógica para processar o pagamento via boleto
     }
